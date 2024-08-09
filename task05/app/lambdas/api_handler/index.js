256535fdd132dd6130d7bfad92fddd9e349be305
suren <tadevosyansuren07@gmail.com>
2024-08-09 15:58:59 +0400

    aws

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -1,10 +1,28 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+
 exports.handler = async (event, context) => {
-    console.log(JSON.stringify(event, null, 2));
-    event.Records.forEach(record => {
-        logDynamoDBRecord(record);
-    });
+    try {
+        console.log(JSON.stringify(event, null, 2));
+
+        // Process each record if applicable
+        event.Records.forEach(record => {
+            logDynamoDBRecord(record);
+        });
+
+        // Return a successful response
+        return {
+            statusCode: 201,
+            body: JSON.stringify({ message: 'Records processed successfully' }),
+        };
+    } catch (error) {
+        // Handle errors and return an appropriate response
+        console.error('Error processing event:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Internal Server Error', error: error.message }),
+        };
+    }
 };
 
 const logDynamoDBRecord = (record) => {
@@ -12,4 +30,3 @@ const logDynamoDBRecord = (record) => {
     console.log(record.eventName);
     console.log(`DynamoDB Record: ${JSON.stringify(record.dynamodb)}`);
 };
-
