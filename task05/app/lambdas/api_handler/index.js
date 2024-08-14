033ad2eabfbf90b76dd9fae8b230eba410b43dda
suren <tadevosyansuren07@gmail.com>
2024-08-14 17:37:55 +0400

    aws

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -1,17 +1,8 @@
-exports.handler = async (event, context) => {
-  console.log(JSON.stringify(event, null, 2));
-
-  if (event.Records && Array.isArray(event.Records)) {
-    event.Records.forEach((record) => {
-      logDynamoDBRecord(record);
-    });
-  } else {
-    console.error("No records found in event.");
-  }
-};
-
-const logDynamoDBRecord = (record) => {
-  console.log(record.eventID);
-  console.log(record.eventName);
-  console.log(`DynamoDB Record: ${JSON.stringify(record.dynamodb)}`);
+exports.handler = async (event) => {
+  // TODO implement
+  const response = {
+    statusCode: 201,
+    body: JSON.stringify("Hello from Lambda!"),
+  };
+  return response;
 };
