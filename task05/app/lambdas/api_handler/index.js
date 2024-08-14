23522bc565c68c4f2f19ec8f3e86a234c3f47dbf
suren <tadevosyansuren07@gmail.com>
2024-08-14 16:47:28 +0400

    Aws

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -1,41 +1,17 @@
-const AWS = require("aws-sdk");
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
-const { v4: uuidv4 } = require("uuid");
+exports.handler = async (event, context) => {
+  console.log(JSON.stringify(event, null, 2));
 
-exports.handler = async (event) => {
-  try {
-    const item = {
-      id: uuidv4(),
-      principalId: parseInt(event.principalId),
-      createdAt: new Date().toISOString(),
-      body: event.body || {},
-    };
-
-    const params = {
-      TableName: "Events",
-      Item: item,
-    };
-
-    await dynamoDB.put(params).promise();
-
-    // Properly return the response object with statusCode
-    return {
-      statusCode: 201,
-      body: JSON.stringify({
-        event: item,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-  } catch (error) {
-    console.error("Error inserting item into DynamoDB:", error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: "Internal Server Error" }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
+  if (event.Records && Array.isArray(event.Records)) {
+    event.Records.forEach((record) => {
+      logDynamoDBRecord(record);
+    });
+  } else {
+    console.error("No records found in event.");
   }
+};
+
+const logDynamoDBRecord = (record) => {
+  console.log(record.eventID);
+  console.log(record.eventName);
+  console.log(`DynamoDB Record: ${JSON.stringify(record.dynamodb)}`);
 };
