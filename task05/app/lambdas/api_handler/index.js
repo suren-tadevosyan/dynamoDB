13d60b8ce60a53eafc18579e87a456d261f4b9e1
suren <tadevosyansuren07@gmail.com>
2024-08-14 14:42:23 +0400

    aws

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -1,14 +1,41 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-exports.handler = async (event, context) => {
-  console.log(JSON.stringify(event, null, 2));
-  event.Records.forEach((record) => {
-    logDynamoDBRecord(record);
-  });
-};
+const AWS = require("aws-sdk");
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const { v4: uuidv4 } = require("uuid");
 
-const logDynamoDBRecord = (record) => {
-  console.log(record.eventID);
-  console.log(record.eventName);
-  console.log(`DynamoDB Record: ${JSON.stringify(record.dynamodb)}`);
+exports.handler = async (event) => {
+  try {
+    const item = {
+      id: uuidv4(),
+      principalId: parseInt(event.principalId),
+      createdAt: new Date().toISOString(),
+      body: event.body || {},
+    };
+
+    const params = {
+      TableName: "Events",
+      Item: item,
+    };
+
+    await dynamoDB.put(params).promise();
+
+    return {
+      statusCode: 201, // Ensure statusCode is always returned
+      body: JSON.stringify({
+        message: "Resource created successfully",
+        item: item,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  } catch (error) {
+    console.error("Error inserting item into DynamoDB:", error);
+    return {
+      statusCode: 500, 
+      body: JSON.stringify({ message: "Internal Server Error" }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  }
 };
