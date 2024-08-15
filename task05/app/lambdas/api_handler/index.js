8cd335552fb3a4b05f9ad96637ebed4682eb7dea
suren <tadevosyansuren07@gmail.com>
2024-08-15 11:29:49 +0400

    aws

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -1,8 +1,44 @@
-exports.handler = async (event) => {
-  // TODO implement
-  const response = {
-    statusCode: 201,
-    body: JSON.stringify("Hello from Lambda!"),
+import AWS from "aws-sdk";
+import { v4 as uuidv4 } from "uuid";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const tableName = process.env.target_table || "Events";
+
+export const handler = async (event) => {
+  const eventId = uuidv4();
+  const createdAt = new Date().toISOString();
+
+  const params = {
+    TableName: tableName,
+    Item: {
+      id: eventId,
+      principalId: event.principalId,
+      createdAt: createdAt,
+      body: event.content,
+    },
   };
-  return response;
+
+  try {
+    const data = await docClient.put(params).promise();
+
+    const res = {
+      statusCode: 201,
+      event: {
+        id: eventId,
+        principalId: event.principalId,
+        createdAt: createdAt,
+        body: event.content,
+      },
+    };
+
+    return res;
+  } catch (error) {
+    return JSON.stringify({
+      statusCode: 500,
+      message: "Could not save event to DynamoDB",
+    });
+  }
 };
